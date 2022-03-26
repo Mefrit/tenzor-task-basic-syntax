ee234b352c002def6be5b9e926d6a31ccf3c44d6
Mefrit <mihail_fokin_1999@mail.ru>
2022-03-27 00:29:37 +0300

    из римской записи в арабскую romanToInteger

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,25 @@
 export function romanToInteger(str) {
-    let result = 0;
+    const numbers_compr = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let result = 0,
+        last_numb = 0;
+    str.split('')
+        .reverse()
+        .forEach((element) => {
+            if (numbers_compr[element] >= last_numb) {
+                result = numbers_compr[element] + result;
+            } else {
+                result = result - numbers_compr[element];
+            }
+            last_numb = numbers_compr[element];
+        });
     /*
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
     return result;
 }
+// expect(romanToInteger('II')).toBe(2);
+//                              ^
+// expect(romanToInteger('XVI')).toBe(16);
+// expect(romanToInteger('XXXVII')).toBe(37);
+// expect(romanToInteger('MMMCDXLIV')).toBe(3444);
